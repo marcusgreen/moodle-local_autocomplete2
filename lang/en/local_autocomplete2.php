966807fc2c460958a0d15e57717d95edfbb0c520
marcusgreen <marcusavgreen@gmail.com>
2019-07-07 22:39:35 +0100

    more updates

---
+++ lang/en/local_autocomplete2.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_autocomplete
+ * @category    string
+ * @copyright   2019 Titus Learning
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'autocomplete2';
+$string['tags'] = 'Tags';
+$string['tags_help'] ='(boolean - default false) Tags allows the selection/creation of new elements not in the dropdown list';
+$string['multiple'] = 'Multple';
+$string['multiple_help'] = '(boolean - default false)  allows the selection and storage of multiple items from the list';
+$string['selectionstring_help'] =" (string - default '') - The text to display when nothing is selected.";
+$string['showsuggestions'] = 'Show suggestions';
+$string['showsuggestions_help'] ="(boolean - default true) - Do not show the list of suggestions when the user starts typing.";
+$string['placeholder_help'] = "(string - default '') - The text to show in the search box when it is empty.";
+$string['casesensitive'] = 'Case sensitive';
+$string['casesensitive_help'] = "(boolean - default false) - Is the search case sensitive ?";
